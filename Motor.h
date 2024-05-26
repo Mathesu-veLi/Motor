06bd8fee3bfec387f5fac12f4555a64de3999b86
Matheus <matheuslevit@gmail.com>
2024-05-26 10:47:56 -0300

    add a overload of the constructor to set a default speed

--- Motor.h
+++ Motor.h
@@ -5,13 +5,18 @@ class Motor {
   private:
     int pinL;
     int pinR;
+    int defaultSpeed;
+    
   public:
     Motor(int pinL, int pinR);
+    Motor(int pinL, int pinR, int defaultSpeed);
 
     void forward();
     void forward(int speed);
+
     void back();
     void back(int speed);
+
     void stop();
 };
 
